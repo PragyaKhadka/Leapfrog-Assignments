3491002393a195bab66b3719e37f7fb1031f73fc
Pragya Khadka <pragyakhadka75@gmail.com>
2022-01-10 10:21:40 +0545

    Jump Move,Start Page and Navbar Update

--- JavaScript Final/bhagchal.js
+++ JavaScript Final/bhagchal.js
@@ -1,16 +1,18 @@
 // import {placegoats} from './utility.js';
+let canvas_bg = document.getElementById("canvas_body")
 let canvas= document.getElementById('canvas');
 let boxes= document.getElementsByClassName('box');
+let gamestatus=document.getElementById('game_status');
 const ctx = canvas.getContext('2d');
 ctx.canvas.width= window.innerWidth;
 ctx.canvas.height= window.innerHeight;
 // ctx.canvas.background = pink;
-let top_pad=100;
+let top_pad=150;
 let left_pad=100;
 let spacing=100;
-let board_height=500;
+let board_height=550;
 let board_width=500;
-let mid_h=300;
+let mid_h=350;
 let mid_w=300;
 let tiger_radius=25;
 let goat_radius=20;
@@ -31,7 +33,7 @@ pieces[4][4]=1;
 console.log(pieces);
 turn=2;
 goats_eaten=0;
-goats_remain=2;
+goats_remain=20;
 goat_clicked=0;
 clickedgoat_row=0;
 clickedgoat_col=0;
@@ -41,16 +43,29 @@ clickedtiger_col=0;
 
 
 function drawline(x1,y1,x2,y2){
+    ctx.lineWidth="2";
     ctx.beginPath();
     ctx.moveTo(x1,y1);
     ctx.lineTo(x2,y2);
     ctx.stroke();
+    
 }
-function drawboard(){
-    ctx.fillStyle="#FF69B4";
+function drawboard(){  
+    var grd = ctx.createLinearGradient(0,0, spacing*4+tiger_radius*2, spacing*4+tiger_radius*2);
+    grd.addColorStop(0.9, "#c19a6b");
+    grd.addColorStop(1,"#80461b");
+    grd.addColorStop(0.7, "#80461b");
+    grd.addColorStop(0.5, "#c19a6b");
+    grd.addColorStop(0.3, "#80461b");
+    ctx.fillStyle = grd;  
+    //ctx.fillStyle="#b38b6d";
+    ctx.lineWidth="10";
+    ctx.strokeStyle="#654321";
+    ctx.rect(left_pad-tiger_radius, top_pad-tiger_radius, spacing*4+tiger_radius*2, spacing*4+tiger_radius*2)
+    ctx.stroke();
     ctx.fillRect(left_pad-tiger_radius, top_pad-tiger_radius, spacing*4+tiger_radius*2, spacing*4+tiger_radius*2)
-    ctx.strokeStyle="white";
-    ctx.strokeRect(left_pad, top_pad, spacing*4, spacing*4);
+    ctx.strokeStyle="skyblue";
+    //ctx.strokeRect(left_pad, top_pad, spacing*4, spacing*4);
     drawline(left_pad,board_height,board_width,top_pad);
     drawline(board_width,board_height,left_pad,top_pad);
     for(let i=0;i<5;i++){
@@ -69,6 +84,7 @@ function drawboard(){
     drawline(mid_w,top_pad,board_width,mid_h);
     
 }
+console.log(window.innerWidth)
 drawboard();
 drawpieces();
 
@@ -77,6 +93,7 @@ function drawpieces(){
         for(j=0;j<5;j++){
             if(pieces[i][j]==1){
                 ctx.beginPath();
+                
                 ctx.fillStyle='#FF7F50';
                 ctx.arc(left_pad+j*spacing,top_pad+i*spacing,25,0,2*Math.PI,true);
                 // ctx.stroke();
@@ -103,31 +120,9 @@ function getmouse_pos(event){
    
     mouse_posX = event.clientX;
 	mouse_posY = event.clientY;
-    // let rect= canvas.getBoundingClientRect();
-    // mouse_posX = event.clientX-rect.left;
-    // mouse_posY = event.clientY-rect.top;
-    // console.log("Coordinate x: " + mouse_posX, 
-    // "Coordinate y: " + mouse_posY);
     mouse_posX = mouse_posX - canvas.offsetLeft;
 	mouse_posY = mouse_posY - canvas.offsetTop;
     return true;
-}
-function distance(x1,y1,x2,y2){
-    let distance=Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2));
-    return(distance);
-}
-function check_valid_move(r1,c1,r2,c2){
-    if(distance(r1,c1,r2,c2)<2){
-        if((r1+c1)%2==0){
-            return 1;
-        }
-        else{
-            if(distance(r1,c1,r2,c2)==1){
-                return 1;
-            }
-        }
-    }
-    return 0;
 }
 function clicked(){
     for(i=0;i<5;i++){
@@ -161,7 +156,7 @@ function clicked(){
             console.log("y1",piece_posY);
             console.log("r",clickedgoat_row);
             console.log("c",clickedgoat_col);
-            if(check_valid_move(clickedgoat_row,clickedgoat_col,piece_posX,piece_posY)==1){
+            if(check_legal_move(clickedgoat_row,clickedgoat_col,piece_posX,piece_posY)==1){
                 console.log("i am in")
                 //console.log(pieces[clickedgoat_row][clickedgoat_col]); 
                 //console.log(pieces[clickedgoat_row][clickedgoat_col]);
@@ -179,25 +174,54 @@ function clicked(){
         clickedtiger_col=piece_posY;
     }
     else if(turn==1 && tiger_clicked==1 && pieces[piece_posX][piece_posY]==0){
-        if(check_valid_move(clickedtiger_row,clickedtiger_col,piece_posX,piece_posY)==1){
+        if(check_legal_move(clickedtiger_row,clickedtiger_col,piece_posX,piece_posY)==1){
             console.log("i am in");
             pieces[piece_posX][piece_posY]=1;
             pieces[clickedtiger_row][clickedtiger_col]=0;
-            goat_clicked = 0;
+            tiger_clicked = 0;
             turn= 2;
             } 
+        else if(check_legal_jump(clickedtiger_row,clickedtiger_col,piece_posX,piece_posY)==1){
+            pieces[piece_posX][piece_posY]=1;
+            pieces[clickedtiger_row][clickedtiger_col]=0;
+            pieces[(piece_posX+clickedtiger_row)/2][(piece_posY+clickedtiger_col)/2]=0;
+            tiger_clicked=0;
+            turn=2;
+            goats_eaten+=1;
+            console.log(goats_eaten);
         }
+    }
+
+
 }
 piece_posX=-1;
 piece_posY=-1;
 drawboard();
 drawpieces();
-// console.log(check_valid_move(2,1,2,2));
+// console.log(check_legal_move(2,1,2,2));
 // // console.log("dist",distance(1,1,0,0));
+let para= document.createElement("paragraph");
+gamestatus.appendChild(para);
+function update(){
+    para.innerHTML=` <p style= "fontFamily:Risque; color:#654321; fontSize:22px;font-family:Risque; font-weight:bold;">&nbsp; &nbsp;Goats On Hand: ${goats_remain}&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Goats Eaten: ${goats_eaten}&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Turn of: ${turnof()}</p>`
+}
+function turnof(){
+    if(turn==1){
+        return `<img style="height=1px; width=1px;position:absolute;top:25px;"src="Images/tigerfinal.png" alt="TIGER">`;
+    }
+    if(turn==2){
+        return `<img style="height=1px; width=1px;position:absolute;left:370px;top:25px;"src="Images/goatfinal.png" alt="GOAT">`;
+    }
+    // if(turn==0){
+    //     return"";
+    //     }
+}
+update();
 function mouseup(){
     drawboard();
     drawpieces();
     if(goat_clicked!=0){
     clicked();
     }
+    update();
 }
--- JavaScript Final/utility.js
+++ JavaScript Final/utility.js
@@ -1,127 +1,43 @@
-// let canvas= document.getElementById('canvas');
-// const ctx = canvas.getContext('2d');
-function drawgoats(x0,y0){
-    ctx.beginPath();
-    ctx.fillStyle='green';
-    ctx.arc(x0,y0,20,0,2*Math.PI);
-    ctx.stroke();
-    ctx.fill();
+function distance(x1,y1,x2,y2){
+    let distance=Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2));
+    return(distance);
+}
+
+function check_legal_move(r1,c1,r2,c2){
+    if(distance(r1,c1,r2,c2)<2){
+        if((r1+c1)%2==0){
+            return 1;
+        }
+        else{
+            if(distance(r1,c1,r2,c2)==1){
+                return 1;
+            }
+        }
     }
-function placegoats(x,y){
-    // if(x==left_pad|| y==top_pad||x==left_pad+1*spacing || y==top_pad+1*spacing||x==left_pad+2*spacing || y==top_pad+2*spacing||x==left_pad+3*spacing || y==top_pad+3*spacing||x==left_pad+4*spacing || y==top_pad+4*spacing){
-    //         drawgoats(x,y);
-    //  }
-    let left_pad=100;
-    let top_pad=100;
-    let spacing=100;
-    //first row
-    // if(!(x>left_pad-10)&&!(x<=left_pad+10)&&!(y>=top_pad-10)&&!(y<=top_pad+10)){
-    //     drawgoats(x,y);
-    //     goat_num+=1;
-    //  }
-    if(x>left_pad+1*spacing-10&&x<=left_pad+1*spacing+10&&y>=top_pad-10&&y<=top_pad+10){
-     drawgoats(x,y);
-     goat_num+=1;
+    return 0;
+}
+
+function check_legal_jump(r1,c1,r2,c2){
+    midpoint_row=(r1+r2)/2;
+    midpoint_col=(c1+c2)/2;
+    if(distance(r1,c1,midpoint_row,midpoint_col)==0.5*distance(r1,c1,r2,c2)){
+        if(check_legal_move(r1,c1,midpoint_row,midpoint_col) && check_legal_move(midpoint_row,midpoint_col,r2,c2) && isgoat(midpoint_row,midpoint_col)){
+            return 1;
+        }
     }
-    if(x>left_pad+2*spacing-10&&x<=left_pad+2*spacing+10&&y>=top_pad-10&&y<=top_pad+10){
-        drawgoats(x,y);
-        goat_num+=1;
-       }
-    if(x>left_pad+3*spacing-10&&x<=left_pad+3*spacing+10&&y>=top_pad-10&&y<=top_pad+10){
-        drawgoats(x,y);
-        goat_num+=1;
-       }
-    // if(!(x>left_pad+4*spacing-10)&&!(x<=left_pad+4*spacing+10)&&!(y>=top_pad-10)&&!(y<=top_pad+10)){
-    //     drawgoats(x,y);
-    //     goat_num+=1;
-    //    }
-    
-    //second row
-    if(x>left_pad-10&&x<=left_pad+10&&y>=top_pad+1*spacing-10&&y<=top_pad+1*spacing+10){
-        drawgoats(x,y);
-        goat_num+=1;
-     }
-    if(x>left_pad+1*spacing-10&&x<=left_pad+1*spacing+10&&y>=top_pad+1*spacing-10&&y<=top_pad+1*spacing+10){
-     drawgoats(x,y);
-     goat_num+=1;
+}
+
+function startgame(){
+    let startpage = document.getElementById("startgame");
+    startpage.style.display="none";
+    canvas_body.style.display="block";
+}
+
+function isgoat(row,col){
+    if(Math.round(row)==row && Math.round(col)== col){
+        if((row>=0 && row<5) && (col>=0&&col<5) && pieces[row][col]==2){
+            return true;
+        }
     }
-    if(x>left_pad+2*spacing-10&&x<=left_pad+2*spacing+10&&y>=top_pad+1*spacing-10&&y<=top_pad+1*spacing+10){
-        drawgoats(x,y);
-        goat_num+=1;
-       }
-    if(x>=left_pad+3*spacing-10&&x<=left_pad+3*spacing+10&&y>=top_pad+1*spacing-10&&y<=top_pad+1*spacing+10){
-        drawgoats(x,y);
-        goat_num+=1;
-       }
-    if(x>left_pad+4*spacing-10&&x<=left_pad+4*spacing+10&&y>=top_pad+1*spacing-10&&y<=top_pad+1*spacing+10){
-        drawgoats(x,y);
-        goat_num+=1;
-       }
-  
-    //third_row
-    if(x>left_pad-10&&x<=left_pad+10&&y>=top_pad+2*spacing-10&&y<=top_pad+2*spacing+10){
-        drawgoats(x,y);
-        goat_num+=1;
-     }
-    if(x>left_pad+1*spacing-10&&x<=left_pad+1*spacing+10&&y>=top_pad+2*spacing-10&&y<=top_pad+2*spacing+10){
-     drawgoats(x,y);
-     goat_num+=1;
-    }
-    if(x>left_pad+2*spacing-10&&x<=left_pad+2*spacing+10&&y>=top_pad+2*spacing-10&&y<=top_pad+2*spacing+10){
-        drawgoats(x,y);
-        goat_num+=1;
-       }
-    if(x>left_pad+3*spacing-10&&x<=left_pad+3*spacing+10&&y>=top_pad+2*spacing-10&&y<=top_pad+2*spacing+10){
-        drawgoats(x,y);
-        goat_num+=1;
-       }
-    if(x>left_pad+4*spacing-10&&x<=left_pad+4*spacing+10&&y>=top_pad+2*spacing-10&&y<=top_pad+2*spacing+10){
-        drawgoats(x,y);
-        goat_num+=1;
-       }
-    
-    //fourth row
-    if(x>left_pad-10&&x<=left_pad+10&&y>=top_pad+3*spacing-10&&y<=top_pad+3*spacing+10){
-        drawgoats(x,y);
-        goat_num+=1;
-     }
-    if(x>left_pad+1*spacing-10&&x<=left_pad+1*spacing+10&&y>=top_pad+3*spacing-10&&y<=top_pad+3*spacing+10){
-     drawgoats(x,y);
-     goat_num+=1;
-    }
-    if(x>left_pad+2*spacing-10&&x<=left_pad+2*spacing+10&&y>=top_pad+3*spacing-10&&y<=top_pad+3*spacing+10){
-        drawgoats(x,y);
-        goat_num+=1;
-       }
-    if(x>left_pad+3*spacing-10&&x<=left_pad+3*spacing+10&&y>=top_pad+3*spacing-10&&y<=top_pad+3*spacing+10){
-        drawgoats(x,y);
-        goat_num+=1;
-       }
-    if(x>left_pad+4*spacing-10&&x<=left_pad+4*spacing+10&&y>=top_pad+3*spacing-10&&y<=top_pad+3*spacing+10){
-        drawgoats(x,y);
-        goat_num+=1;
-       }
-    
-    
-    //fifth row
-    // if(!(x>left_pad-10)&&!(x<=left_pad+10)&&!(y>=top_pad+4*spacing-10)&&!(y<=top_pad+4*spacing+10)){
-    //     drawgoats(x,y);
-    //  }
-    if(x>left_pad+1*spacing-10&&x<=left_pad+1*spacing+10&&y>=top_pad+4*spacing-10&&y<=top_pad+4*spacing+10){
-     drawgoats(x,y);
-     goat_num+=1;
-    }
-    if(x>left_pad+2*spacing-10&&x<=left_pad+2*spacing+10&&y>=top_pad+4*spacing-10&&y<=top_pad+4*spacing+10){
-        drawgoats(x,y);
-        goat_num+=1;
-       }
-    if(x>left_pad+3*spacing-10&&x<=left_pad+3*spacing+10&&y>=top_pad+4*spacing-10&&y<=top_pad+4*spacing+10){
-        drawgoats(x,y);
-        goat_num+=1;
-       }
-    // if(!(x>left_pad+4*spacing-10)&&!(x<=left_pad+4*spacing+10)&&!(y>=top_pad+4*spacing-10)&&!(y<=top_pad+4*spacing+10)){
-    //     drawgoats(x,y);
-    //    }
-    
-    console.log(goat_num);
+
 }
