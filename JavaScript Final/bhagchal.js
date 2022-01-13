90bc58c52a0af5061214a55715ed36169903d905
Pragya Khadka <pragyakhadka75@gmail.com>
2022-01-13 12:58:39 +0545

    New updates pvp and cvp

--- JavaScript Final/bhagchal.js
+++ JavaScript Final/bhagchal.js
@@ -1,12 +1,13 @@
-// import {placegoats} from './utility.js';
-let canvas_bg = document.getElementById("canvas_body")
+let canvas_bg = document.getElementById("canvas_body");
 let canvas= document.getElementById('canvas');
-let boxes= document.getElementsByClassName('box');
+let startpage = document.getElementById("startgame");
 let gamestatus=document.getElementById('game_status');
+let gametype=document.getElementById("choose_gametype");
+let notification = document.getElementById("notify");
 const ctx = canvas.getContext('2d');
 ctx.canvas.width= window.innerWidth;
 ctx.canvas.height= window.innerHeight;
-// ctx.canvas.background = pink;
+
 let top_pad=150;
 let left_pad=100;
 let spacing=100;
@@ -31,7 +32,6 @@ pieces[0][4]=1;
 pieces[4][0]=1;
 pieces[4][4]=1;
 console.log(pieces);
-turn=2;
 goats_eaten=0;
 goats_remain=20;
 goat_clicked=0;
@@ -40,7 +40,12 @@ clickedgoat_col=0;
 tiger_clicked=0;
 clickedtiger_row=0;
 clickedtiger_col=0;
-
+turn=2;
+function game(n){
+game_type=n;
+drawboard();
+drawpieces();
+}
 
 function drawline(x1,y1,x2,y2){
     ctx.lineWidth="2";
@@ -48,7 +53,6 @@ function drawline(x1,y1,x2,y2){
     ctx.moveTo(x1,y1);
     ctx.lineTo(x2,y2);
     ctx.stroke();
-    
 }
 function drawboard(){  
     var grd = ctx.createLinearGradient(0,0, spacing*4+tiger_radius*2, spacing*4+tiger_radius*2);
@@ -59,13 +63,13 @@ function drawboard(){
     grd.addColorStop(0.3, "#80461b");
     ctx.fillStyle = grd;  
     //ctx.fillStyle="#b38b6d";
-    ctx.lineWidth="10";
+    //ctx.lineWidth="10";
     ctx.strokeStyle="#654321";
-    ctx.rect(left_pad-tiger_radius, top_pad-tiger_radius, spacing*4+tiger_radius*2, spacing*4+tiger_radius*2)
-    ctx.stroke();
+    ctx.strokeRect(left_pad-tiger_radius, top_pad-tiger_radius, spacing*4+tiger_radius*2, spacing*4+tiger_radius*2)
+    // ctx.stroke();
     ctx.fillRect(left_pad-tiger_radius, top_pad-tiger_radius, spacing*4+tiger_radius*2, spacing*4+tiger_radius*2)
-    ctx.strokeStyle="skyblue";
-    //ctx.strokeRect(left_pad, top_pad, spacing*4, spacing*4);
+    ctx.strokeStyle="#28282B";
+    ctx.strokeRect(left_pad, top_pad, spacing*4, spacing*4);
     drawline(left_pad,board_height,board_width,top_pad);
     drawline(board_width,board_height,left_pad,top_pad);
     for(let i=0;i<5;i++){
@@ -81,35 +85,52 @@ function drawboard(){
     drawline(left_pad,mid_h,mid_w,top_pad);
     drawline(left_pad,mid_h,mid_w,board_height);
     drawline(mid_w,board_height,board_width,mid_h);
-    drawline(mid_w,top_pad,board_width,mid_h);
-    
+    drawline(mid_w,top_pad,board_width,mid_h);      
+    // drawpieces();  
 }
-console.log(window.innerWidth)
-drawboard();
-drawpieces();
+let tiger_image= new Image();
+tiger_image.onload=drawtiger();
+//tiger_image.onerror=function(){alert(img1.src+' failed to load.');};
+tiger_image.src="Images/tigerfinal.png";
+let goat_image= new Image();
+goat_image.onload=drawgoat();
+goat_image.src="Images/goatfinal.png";
+
+function drawtiger(i,j){
+    ctx.drawImage(tiger_image,left_pad+j*spacing-30,top_pad+i*spacing-30);
+}
+function drawgoat(i,j){
+
+    ctx.drawImage(goat_image,left_pad+j*spacing-40,top_pad+i*spacing-25);
+}
 
 function drawpieces(){
+    let goat_image= new Image();
+    goat_image.src="Images/goatfinal.png";
     for(i=0;i<5;i++){
         for(j=0;j<5;j++){
             if(pieces[i][j]==1){
-                ctx.beginPath();
-                
-                ctx.fillStyle='#FF7F50';
-                ctx.arc(left_pad+j*spacing,top_pad+i*spacing,25,0,2*Math.PI,true);
-                // ctx.stroke();
-                ctx.fill();
+                drawtiger(i,j);
+                //  ctx.beginPath();
+                // ctx.fillStyle='#FF7F50';
+                // ctx.arc(left_pad+j*spacing,top_pad+i*spacing,25,0,2*Math.PI,true);
+                // // ctx.stroke();
+                // ctx.fill();
+               
         }
         else if(pieces[i][j]==2){
-            ctx.beginPath();
-            ctx.fillStyle='green';
-            ctx.arc(left_pad+j*spacing,top_pad+i*spacing,20,0,2*Math.PI,true);
-            // ctx.stroke();
-            ctx.fill();
+            drawgoat(i,j);
+           // ctx.drawImage(goat_image,left_pad+j*spacing-40,top_pad+i*spacing-25);
+            // ctx.beginPath();
+            // ctx.fillStyle='green';
+            // ctx.arc(left_pad+j*spacing,top_pad+i*spacing,20,0,2*Math.PI,true);
+            // ctx.fill();
         }
         }
     } 
 }
-//drawpieces();
+drawboard();
+drawpieces();
 document.onmousemove = getmouse_pos;
 canvas.onmousedown = clicked;
 canvas.onmouseup = mouseup;
@@ -117,29 +138,33 @@ let mouse_posX= 0;
 let mouse_posY= 0;
 
 function getmouse_pos(event){
-   
     mouse_posX = event.clientX;
 	mouse_posY = event.clientY;
     mouse_posX = mouse_posX - canvas.offsetLeft;
 	mouse_posY = mouse_posY - canvas.offsetTop;
     return true;
 }
+let click_sound= new Audio("Sounds/Mouseclick.mp3");
+click_sound.volume=0.1;
+let victory_sound= new Audio("Sounds/victoryff.swf.mp3");
+victory_sound.volume=0.1;
+let tiger_sound= new Audio("Sounds/tigersnarl.mp3");
+tiger_sound.volume=1;
 function clicked(){
     for(i=0;i<5;i++){
         for(j=0;j<5;j++){
             pos_x = left_pad+j*spacing;
             pos_y = top_pad+i*spacing;
-            //console.log(pos_x);
             if((mouse_posX>pos_x-goat_radius)&&(mouse_posX<pos_x+goat_radius)&&(mouse_posY>pos_y-goat_radius)&&(mouse_posY<pos_y+goat_radius)){
                 piece_posX=i;
-                piece_posY=j;
-                console.log("x",piece_posX)
-                console.log("y",piece_posY)
+                piece_posY=j;   
+                click_sound.play();
             }
 
         }
     }
     if(piece_posX>-1){
+        if(game_type==1 || game_type==2){
         if(turn==2 && goats_remain>0 && pieces[piece_posX][piece_posY]==0){
             goats_remain = goats_remain-1;
             pieces[piece_posX][piece_posY]=2;
@@ -149,15 +174,9 @@ function clicked(){
             goat_clicked= 1;
             clickedgoat_row=piece_posX;
             clickedgoat_col=piece_posY;
-            
         }
         else if(turn==2 && goat_clicked==1 && pieces[piece_posX][piece_posY]==0){
-            console.log("x1",piece_posX);
-            console.log("y1",piece_posY);
-            console.log("r",clickedgoat_row);
-            console.log("c",clickedgoat_col);
             if(check_legal_move(clickedgoat_row,clickedgoat_col,piece_posX,piece_posY)==1){
-                console.log("i am in")
                 //console.log(pieces[clickedgoat_row][clickedgoat_col]); 
                 //console.log(pieces[clickedgoat_row][clickedgoat_col]);
                 pieces[piece_posX][piece_posY]=2;
@@ -165,23 +184,27 @@ function clicked(){
                 goat_clicked = 0;
                 turn= 1;
                 } 
+            }
         }
-
     }
-    if(turn==1&&pieces[piece_posX][piece_posY]==1){
+
+    if(game_type==1){
+    if(turn==1 && pieces[piece_posX][piece_posY]==1){
+        console.log(piece_posX,piece_posY);
         tiger_clicked=1;
         clickedtiger_row=piece_posX;
         clickedtiger_col=piece_posY;
     }
     else if(turn==1 && tiger_clicked==1 && pieces[piece_posX][piece_posY]==0){
         if(check_legal_move(clickedtiger_row,clickedtiger_col,piece_posX,piece_posY)==1){
-            console.log("i am in");
+            //console.log("i am in");
             pieces[piece_posX][piece_posY]=1;
             pieces[clickedtiger_row][clickedtiger_col]=0;
             tiger_clicked = 0;
             turn= 2;
             } 
         else if(check_legal_jump(clickedtiger_row,clickedtiger_col,piece_posX,piece_posY)==1){
+            tiger_sound.play();
             pieces[piece_posX][piece_posY]=1;
             pieces[clickedtiger_row][clickedtiger_col]=0;
             pieces[(piece_posX+clickedtiger_row)/2][(piece_posY+clickedtiger_col)/2]=0;
@@ -191,15 +214,46 @@ function clicked(){
             console.log(goats_eaten);
         }
     }
-
-
 }
-piece_posX=-1;
-piece_posY=-1;
-drawboard();
-drawpieces();
-// console.log(check_legal_move(2,1,2,2));
-// // console.log("dist",distance(1,1,0,0));
+if(game_type==2 && turn==1){
+        tiger_bestmove();
+    }
+    piece_posX=-1;
+    piece_posY=-1;
+
+let winner = decide_winner();
+    if(winner==1 && turn==2){
+        notify();
+        }
+        else if(winner==2 && turn==1){
+        notify();
+        }
+        drawboard();
+        drawpieces();
+        update();
+}
+
+function display_winnerimage(){
+    let winners = decide_winner();
+    if(winners==1){
+            return `<img src="Images/crownedt.png" style="height:80px; width:80px; position:absolute; left:500px;top:100px">`;
+            
+    }
+    else if(winners==2){
+        return `<img src="Images/crownedg.png" style="height:80px; width:80px; position:absolute; left:500px;top:100px">`;
+        }
+}
+
+function display_winner(){
+    let winners = decide_winner();
+    if(winners==1){
+        return "THE TIGERS"
+    }
+    else if(winners==2){
+            return "THE GOATS";
+        }
+}
+
 let para= document.createElement("paragraph");
 gamestatus.appendChild(para);
 function update(){
@@ -212,16 +266,38 @@ function turnof(){
     if(turn==2){
         return `<img style="height=1px; width=1px;position:absolute;left:370px;top:25px;"src="Images/goatfinal.png" alt="GOAT">`;
     }
-    // if(turn==0){
-    //     return"";
-    //     }
+    if(turn==0){
+        return"";
+        }
 }
 update();
+drawpieces();
 function mouseup(){
-    drawboard();
-    drawpieces();
-    if(goat_clicked!=0){
+   drawboard();
+   drawpieces();
+    if(tiger_clicked!=0||goat_clicked!=0){
     clicked();
     }
     update();
+}
+function decide_winner(){
+ if (goats_eaten>=5){
+     return 1;   
+ }
+ for(r1=0;r1<5;r1++){
+     for(c1=0;c1<5;c1++){
+         if(pieces[r1][c1]==1){
+            for(r2=0;r2<5;r2++){
+                for(c2=0;c2<5;c2++){
+                    if(pieces[r2][c2]==0){
+                        if(check_legal_jump(r1,c1,r2,c2)==1||check_legal_move(r1,c1,r2,c2)==1){
+                        return 0;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+return 2;
 }
--- JavaScript Final/utility.js
+++ JavaScript Final/utility.js
@@ -28,9 +28,32 @@ function check_legal_jump(r1,c1,r2,c2){
 }
 
 function startgame(){
-    let startpage = document.getElementById("startgame");
+    startpage.style.display="none";
+    canvas_body.style.display="none";
+    notification.style.display="none";
+    gametype.style.display="block";
+}
+
+function choose_game(){
     startpage.style.display="none";
     canvas_body.style.display="block";
+    notification.style.display="none";
+    gametype.style.display="none";
+}
+function isblank(row,col){
+    if(Math.round(row)==row && Math.round(col)== col){
+        if((row>=0 && row<5) && (col>=0&&col<5) && pieces[row][col]==0){
+            return true;
+        }
+    }
+}
+
+function istiger(row,col){
+    if(Math.round(row)==row && Math.round(col)== col){
+        if((row>=0 && row<5) && (col>=0&&col<5) && pieces[row][col]==1){
+            return true;
+        }
+    }
 }
 
 function isgoat(row,col){
@@ -39,5 +62,86 @@ function isgoat(row,col){
             return true;
         }
     }
-
+}
+function reload(){
+        location.reload();
+}
+function notify(){
+    victory_sound.play();
+    turn=0;
+    let p_notify = document.createElement("p")
+    notification.appendChild(p_notify);
+    p_notify.innerHTML=`${display_winnerimage()}<br>
+                        <p style="color:#6B8E23;font-size:28px;font-family:Risque;position:absolute; left:350px;top:180px;"> ${display_winner()} WIN THE MATCH...</p><br>
+                        <p style="color:#AB6B51;font-size:22px;font-family:Risque;position:absolute; left:330px;top:220px;">[Click on the New Game Button to Play Again]</p>`;
+    startpage.style.display="none";
+    canvas_body.style.display="block";
+    notification.style.display="block";
+    console.log("in notify");
+}
+function tiger_bestmove(){
+    max_point=0;
+    max_r1=0; max_c1=0; max_r2=0; max_c2=0;
+   
+        for(r1=0;r1<5;r1++){
+        for(c1=0;c1<5;c1++){
+            for(r2=0;r2<5;r2++){
+                for(c2=0;c2<5;c2++){
+                    let points=0;
+                    if(istiger(r1,c1) && isblank(r2,c2)){
+                        if(check_legal_jump(r1,c1,r2,c2)==1){
+                            points = points+10000;
+                            points = points+ evaluate_move(r1,c1,r2,c2);
+                        }
+                        if(check_legal_move(r1,c1,r2,c2)==1){
+                            points = points+30;
+                            points= points+ evaluate_move(r1,c1,r2,c2);
+                        }
+                        if(points>=max_point){
+                            max_r1=r1;
+                            max_r2=r2;
+                            max_c1=c1;
+                            max_c2=c2;
+                            max_point=points;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    if(max_point!=0){
+        pieces[max_r1][max_c1]=0;
+        pieces[max_r2][max_c2]=1;
+        if(max_point>=1000){
+            pieces[(max_r1+max_r2)/2][(max_c1+max_c2)/2]=0;
+            goats_eaten=goats_eaten+1;
+            tiger_sound.play();
+        }
+        turn=2;
+    }
+}
+function evaluate_move(){
+    score=0;
+    for(i=r2-2;i<=r2+2;i=i+2){
+        for(j=c2-2;j<=c2+2;j=j+2){
+            if(check_legal_jump(r2,c2,i,j)==1 && isblank(i,j)){
+                score=score+100;
+            } 
+        }
+    }
+    for(i=r1-2;i<=r1+2;i=i+2){
+        for(j=c1-2;j<=c1+2;j=j+2){
+            if(check_legal_move(i,j,r1,c1)==1 && istiger(i,j)){
+                score=score+50;
+            }
+        }
+    }
+if((r2+c2)%2==0){
+    score = score+10;
+}
+if(r2==0||r2==4||c2==0||c2==4){
+    score = score+5;
+}
+score = score+Math.abs(Math.pow(r2-2,3)+Math.pow(c2-2,3));
+return score;
 }
